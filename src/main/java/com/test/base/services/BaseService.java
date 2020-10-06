bed7570049e1e2bc79d6f1fa01ef116131ab41a7
abbas <ghaisarabbas@gmail.com>
2020-10-06 19:16:56 +0330

    add  BaseService and BaseServiceImpl to createEntities branch

---
+++ src/main/java/com/test/base/services/BaseService.java
@@ -0,0 +1,20 @@
+package com.test.base.services;
+
+import com.test.base.domains.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseService<E extends BaseEntity<PK>, PK extends Serializable> {
+    Optional<E> save(E e);
+
+    Optional<E> update(E e);
+
+    Optional<E> findById(Class<E> c, PK id);
+
+    void delete(Class<E> c, PK id);
+
+    List<E> findAll(Class<E> c, String query);
+
+}
